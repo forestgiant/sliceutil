46dc777c1146a2c07624fc60458d33ce3c8dcda2
Stephan Baker <stephan.baker@gmail.com>
2016-02-08 23:43:56 -0500

    Adding OrderedCompare function.

--- sliceutil_test.go
+++ sliceutil_test.go
@@ -28,3 +28,33 @@ func TestCompare(t *testing.T) {
 		}
 	}
 }
+
+func TestOrderedCompare(t *testing.T) {
+	var tests = []struct {
+		s1     interface{}
+		s2     interface{}
+		result bool
+	}{
+		{[]string{"string1", "string2"}, []string{"string1", "string2"}, true},
+		{[]string{"string1", "string2"}, []string{"string2", "string1"}, false},
+		{[]string{"string3", "string2"}, []string{"string2", "string1"}, false},
+		{[]string{"string3", "string2", "string1"}, []string{"string2", "string1"}, false},
+		{[]string{"string2", "string2", "string1"}, []string{"string2", "string1"}, false},
+		{[]int{1, 2}, []int{1, 2}, true},
+		{[]int{1, 2}, []int{2, 1}, false},
+		{[]int{3, 2}, []int{2, 1}, false},
+		{nil, nil, true},
+		{nil, []int{2, 1}, false},
+		{[]bool{true, false}, []bool{true, false}, true},
+		{[]int{1, 0}, []bool{true, false}, false},
+		{true, []bool{true, false}, false},
+		{true, true, false},
+	}
+
+	for _, test := range tests {
+		actual := OrderedCompare(test.s1, test.s2)
+		if actual != test.result {
+			t.Errorf("(%q,%q) = %v; want %v", test.s1, test.s2, actual, test.result)
+		}
+	}
+}
